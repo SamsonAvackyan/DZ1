c4338e06542c2568ea38c87b0e42a0ed7658d2e0
Samson <samsonavackyan@gmail.com>
2019-06-02 20:34:04 +0300

    Итоговый вариант

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 'use strict';
 var money = prompt("Ваш бюджет на месяц?");
-var time = 
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+var appData = {
+    budjet: money,  
+    timeData: time,
+    expenses: {"a1":"a2"},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+var a1 = prompt("Введите обязательную статью расходов в этом месяце");
+var a2 = prompt("Во сколько обойдется?");
+var a1 = prompt("Введите обязательную статью расходов в этом месяце");
+var a2 = prompt("Во сколько обойдется?");
+alert(money/30);
